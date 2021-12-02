93700d8f156b6c568d64a042e113cec8dc709930
David Bustos <david.bustosonate@gmail.com>
2021-12-02 01:23:50 -0300

    Update app_cv2.py

--- app_cv2.py
+++ app_cv2.py
@@ -5,20 +5,12 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("mdl_wts.hdf5")
+model = tf.keras.models.load_model("model.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
-map_dict = {0: 'dog',
-            1: 'horse',
-            2: 'elephant',
-            3: 'butterfly',
-            4: 'chicken',
-            5: 'cat',
-            6: 'cow',
-            7: 'sheep',
-            8: 'spider',
-            9: 'squirrel'}
+map_dict = {0: 'healthy',
+            1: 'anomalous'}
 
 
 if uploaded_file is not None:
@@ -26,7 +18,7 @@ if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(opencv_image,(224,224))
+    resized = cv2.resize(opencv_image,(160,160))
     # Now do something with the image! For example, let's display it:
     st.image(opencv_image, channels="RGB")
 
